cc49f23a39373f65eec93e461e6b955755721311
Samuel Atsyatsya <samuel.atsyatsya@amalitechtraining.org>
2026-06-24 07:35:00 +0000

    fix(terraform/eks): replace semicolons with newlines in variable blocks

--- terraform/environments/eks/variables.tf
+++ terraform/environments/eks/variables.tf
@@ -23,6 +23,17 @@ variable "node_instance_types" {
   default = ["t3.medium"]
 }
 
-variable "node_desired_size" { type = number; default = 2 }
-variable "node_min_size" { type = number; default = 1 }
-variable "node_max_size" { type = number; default = 5 }
+variable "node_desired_size" {
+  type    = number
+  default = 2
+}
+
+variable "node_min_size" {
+  type    = number
+  default = 1
+}
+
+variable "node_max_size" {
+  type    = number
+  default = 5
+}
